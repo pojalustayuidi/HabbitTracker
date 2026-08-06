b1ee627f90674fce45d342bf61447741c997cb31
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-08-07 01:15:35 +0300

    v0.01. Create first static home screen layout

--- app/src/main/java/com/example/habbittracker/MainActivity.kt
+++ app/src/main/java/com/example/habbittracker/MainActivity.kt
@@ -1,47 +1,81 @@
-        package com.example.habbittracker
+            package com.example.habbittracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.habbittracker.ui.theme.HabbitTrackerTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HabbitTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+           HomeScreen()
+
+        }
+
+    }
+
+
+    @Composable
+    fun GreetingSection(name: String, streak: Int, modifier: Modifier = Modifier) {
+        Column {
+            Text(
+                text = "Привет $name!",
+                modifier = modifier
+            )
+            Text(
+                text = "$streak  Стрик",
+                modifier = modifier
+            )
+        }
+    }
+
+
+@Composable
+fun CardLevel(level: Int, xp: Int){
+    Column(Modifier.background(color = Color.Gray)) {
+        Text(text = "Уровень $level")
+
+        Text(text = "$xp / 1000 XP")
+    }}
+
+
+        @Composable
+        fun HabitItem(checkbox: Char, habbit: String, xp: Int){
+            Row(Modifier.background(color = Color.Red)) {
+                Text(text =  "$checkbox", Modifier.padding(horizontal = 10.dp))
+                Text(text = habbit)
+                Text(text = "+$xp XP") }}
+
+
+    @Composable
+    fun HomeScreen() {
+        Column {
+            Spacer(modifier = Modifier.height(100.dp))
+
+            GreetingSection(name = "Артём", streak = 10, modifier = Modifier.padding(bottom = 5.dp))
+            Spacer(modifier = Modifier.height(100.dp))
+            CardLevel(xp = 100, level = 2)
+            Spacer(modifier = Modifier.height(100.dp))
+            HabitItem(xp = 10, checkbox = '☐', habbit = "Пробежка")
+            HabitItem(xp = 25, checkbox = '☐', habbit = "Сигареты")
+            HabitItem(xp = 10, checkbox = '☐', habbit = "Душ")
+
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HabbitTrackerTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/habbittracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/habbittracker/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.habbittracker.ui.theme
+    package com.example.habbittracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
