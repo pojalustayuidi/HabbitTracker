c76b9507d9ece63b61d58684b18e820294bc50d1
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-08-21 01:56:23 +0300

    Add custom light/dark color theme based on CoinHabit design

--- app/src/main/java/com/example/habbittracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/habbittracker/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.example.habbittracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val HabitGreen = Color(0xFF15803D)
+val HabitGreenLight = Color(0xFFDCFCE7)
+val HabitBackground = Color(0xFFF8F7F3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val HabitSurface = Color(0xFFFFFFFF)
+val HabitTextPrimary = Color(0xFF172018)
+val HabitTextSecondary = Color(0xFF667067)
+
+val HabitAccent = Color(0xFFF59E0B)
+val HabitFire = Color(0xFFF97316)
+
+val HabitBorder = Color(0xFFE5E7DF)
+
+
+val HabitGreenDark = Color(0xFF1FBA63)
+val HabitGreenLightDark = Color(0xFF26D07C)
+val HabitBackgroundDark = Color(0xFF0F1115)
+val HabitSurfaceDark = Color(0xFF161A21)
+val HabitTextPrimaryDark = Color(0xFFF2F2F4)
+val HabitTextSecondaryDark = Color(0xFFA1A7B3)
+val HabitAccentDark = Color(0xFFFFB800)
+val HabitFireDark = Color(0xFFFF5A1F)
+val HabitBorderDark = Color(0xFF2A2F3A)
