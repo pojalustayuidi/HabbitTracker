cae02779c3a6ece80aabee9a5f12baf809ccd324
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-08-27 01:02:12 +0300

    add Room annotations to Habit entity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.room.common)
     implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
