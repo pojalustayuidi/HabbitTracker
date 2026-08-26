ac81bf4209a933a032cb25028a7e2c497d070474
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-08-27 01:13:25 +0300

    add HabitDao with Flow and suspend functions

---
+++ app/src/main/java/com/example/habbittracker/data/HabitDao.kt
@@ -0,0 +1,17 @@
+package com.example.habbittracker.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.habbittracker.model.Habit
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface HabitDao {
+    @Query("SELECT * FROM habits_table")
+    fun getAllHabits():Flow<List<Habit>>
+
+    @Insert
+    suspend fun insertHabit(habit: Habit)
+
+}
