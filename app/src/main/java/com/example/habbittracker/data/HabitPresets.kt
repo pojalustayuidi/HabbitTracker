e724af2083db2787565db352aec00befefa3a18c
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-01 00:54:28 +0300

    add default habit presets list

---
+++ app/src/main/java/com/example/habbittracker/data/HabitPresets.kt
@@ -0,0 +1,43 @@
+package com.example.habbittracker.data
+
+object HabitPresets{
+    val defaultHabits = listOf<HabitItemModel>(
+        HabitItemModel(
+            id = 1,
+            type = HabitType.BAD_HABIT,
+            title = "Сигареты",
+            rewardAmount = 1,
+        ) ,
+        HabitItemModel(
+            id = 2,
+        type = HabitType.BAD_HABIT,
+        title = "Энергетики",
+        rewardAmount = 1,
+    ),
+        HabitItemModel(
+            id = 3,
+            type = HabitType.BAD_HABIT,
+            title = "Вейп",
+            rewardAmount = 1,
+        ),
+        HabitItemModel(
+            id = 4,
+            type = HabitType.BAD_HABIT,
+            title = "Алкоголь",
+            rewardAmount = 1,
+        ),
+        HabitItemModel(
+            id = 5,
+            type = HabitType.GOOD_HABIT,
+            title = "Чтение книг",
+            rewardAmount = 2,
+        ),
+        HabitItemModel(
+            id = 6,
+            type = HabitType.GOOD_HABIT,
+            title = "Пить воду",
+            rewardAmount = 2,
+        )
+
+    )
+}
