6a9fa8a0b3d95827ee04dd7edd5e0bd7006bbf53
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-01 00:35:26 +0300

    add basic layout for habit selection screen

---
+++ app/src/main/java/com/example/habbittracker/ui/components/HabitsCard.kt
@@ -0,0 +1,12 @@
+package com.example.habbittracker.ui.components
+
+import androidx.compose.runtime.Composable
+import org.w3c.dom.Text
+
+@Composable
+fun HabitsCard(
+    Habit: Text
+
+){
+
+}
--- app/src/main/java/com/example/habbittracker/ui/screens/HabitSelection.kt
+++ app/src/main/java/com/example/habbittracker/ui/screens/HabitSelection.kt
@@ -1,13 +1,26 @@
 package com.example.habbittracker.ui.screens
 
+import android.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.habbittracker.ui.theme.HabitTextPrimary
 
 @Composable
 fun HabitSection(onNextClick: () -> Unit) {
     Column() {
-        Text("fwe")
+        Text("Что хочешь изменить?",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+
+            )
+        Text("Выбери одну или несколько привычек",
+        fontSize = 12.sp,
+color = HabitTextPrimary
+        )
 
 
     }
