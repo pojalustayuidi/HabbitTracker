ac68f5cdd5b3b8d0993221e035d3f3817431818c
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-01 00:38:15 +0300

    clear unnecessary

--- app/src/main/java/com/example/habbittracker/ui/screens/HabitSelection.kt
+++ app/src/main/java/com/example/habbittracker/ui/screens/HabitSelection.kt
@@ -1,17 +1,15 @@
 package com.example.habbittracker.ui.screens
 
-import android.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.habbittracker.ui.theme.HabitTextPrimary
 
 @Composable
 fun HabitSection(onNextClick: () -> Unit) {
-    Column() {
+    Column {
         Text("Что хочешь изменить?",
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold,
