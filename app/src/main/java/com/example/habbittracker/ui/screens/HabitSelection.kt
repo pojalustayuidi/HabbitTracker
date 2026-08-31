c09bbd00dc74d73c7f0c0a062591551d979e20a0
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-01 00:17:01 +0300

    add habit_selection route and connect welcome screen

---
+++ app/src/main/java/com/example/habbittracker/ui/screens/HabitSelection.kt
@@ -0,0 +1,14 @@
+package com.example.habbittracker.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HabitSection(onNextClick: () -> Unit) {
+    Column() {
+        Text("fwe")
+
+
+    }
+}
