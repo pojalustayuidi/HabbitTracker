3ccbd194b2781b583562383be6dd185f5772be7b
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-01 22:15:45 +0300

    add polished HabitsCard with centered text and padding

--- app/src/main/java/com/example/habbittracker/ui/components/HabitsCard.kt
+++ app/src/main/java/com/example/habbittracker/ui/components/HabitsCard.kt
@@ -1,12 +1,18 @@
 package com.example.habbittracker.ui.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.habbittracker.ui.theme.HabitGreen
@@ -22,9 +28,20 @@ onClick: () -> Unit
 
         colors = CardDefaults.cardColors(
             containerColor = if (isSelected) HabitGreen else MaterialTheme.colorScheme.surfaceVariant),
-        modifier = Modifier.height(100.dp).clickable{onClick()}
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            .height(100.dp)
+            .clickable { onClick() }
            ){
-        Text(title)
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ){
+            Text(title, )
+        }
 
     }
 }
