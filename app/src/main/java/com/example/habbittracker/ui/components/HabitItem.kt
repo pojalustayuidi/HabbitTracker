1cbcff690aa1a9c42c06b99e3d38a95c420c86b8
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-02 00:05:39 +0300

    save local changes in HabitItem

--- app/src/main/java/com/example/habbittracker/ui/components/HabitItem.kt
+++ app/src/main/java/com/example/habbittracker/ui/components/HabitItem.kt
@@ -1,64 +1,124 @@
 package com.example.habbittracker.ui.components
 
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Checkbox
+import androidx.compose.material.icons.rounded.CheckCircle
+import androidx.compose.material.icons.rounded.RadioButtonUnchecked
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.habbittracker.model.Habit
+import com.example.habbittracker.ui.theme.HabitGreen
+import com.example.habbittracker.ui.theme.HabitGreenLight
 import com.example.habbittracker.ui.theme.HabitTextSecondary
 
 @Composable
 fun HabitItem(habit: Habit, onToggle: () -> Unit, onDelete: () -> Unit){
     OutlinedCard(
-        modifier = Modifier.padding(vertical = 4.dp)
+        modifier = Modifier.padding(vertical = 8.dp)
 
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-//                    horizontalArrangement = Arrangement.SpaceBetween,
-            modifier =  Modifier
-                .padding(5.dp)
-                .fillMaxWidth()
-        )  {
+        Column(modifier = Modifier.padding(
+            vertical = 12.dp
+        )) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                //                    horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .size(48.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(color = Color.Red)
 
-            Checkbox(modifier = Modifier.padding(horizontal = 6.dp),
+                )
+                {
+                    Icon(
+                        contentDescription = "Иконка привычки", imageVector = Icons.Rounded.Star,
+                        modifier = Modifier.size(40.dp)
+                    )
 
-                checked = habit.done, onCheckedChange = {onToggle()}) //false → true
-            if (!habit.done){
-                Column() {Text(text = habit.name)
-                    Text(text = "5/7 дней", color = HabitTextSecondary, fontSize = 12.sp)
+                }
+
+                if (!habit.done) {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(text = habit.name)
+                        Text(text = "5/7 дней", color = HabitTextSecondary, fontSize = 12.sp)
+                    }
+                } else {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(
+                            text = habit.name,
+                            modifier = Modifier.alpha(0.5f),
+                            textDecoration = TextDecoration.LineThrough
+                        )
+                        Text(text = "+${habit.xp} XP", color = HabitGreen, fontSize = 12.sp)
+                    }
+                }
+                IconButton(onClick = onToggle) {
+                    Icon(
+                        imageVector = if (habit.done) {
+                            Icons.Rounded.CheckCircle // Заполненный закругленный круг с галочкой
+                        } else {
+                            Icons.Rounded.RadioButtonUnchecked // Пустой аккуратный контур
+                        },
+                        contentDescription = "Отметить привычку",
+                        tint = if (habit.done) HabitGreen else HabitTextSecondary,
+                        modifier = Modifier.size(28.dp)
+                    )
+                }
+
+
+                IconButton(
+                    onClick = {
+                        //будет редактириование
+                    },
+                )
+                {      Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Удалить привычку")
                 }
 
             }
-            else {
-                Text(text = habit.name, modifier = Modifier.alpha(0.5f), textDecoration = TextDecoration.LineThrough )
-                Text(text =  "+ ${habit.xp} HP ", Modifier.padding(start = 25.dp))}
-            IconButton(
-                onClick = {
-//будет редактириование
-                },
-            ){
-                Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Удалить привычку")
-            }
+            LinearProgressIndicator(
+                progress = { 0.7f },
+                modifier = Modifier
+                    .height(8.dp)
+                    .padding(horizontal = 70.dp)
+                    .fillMaxWidth(),
 
-
-
-        }
+                color = HabitGreen,
+                trackColor = HabitGreenLight,
+                strokeCap = StrokeCap.Round,
+                drawStopIndicator = {},
+                gapSize = 0.dp
+            ) }
 
     }
 
