8e5899e0a8196e366dd866fa334f0b7bbf14ab2e
Комаров Егор <123303256+pojalustayuidi@users.noreply.github.com>
2026-09-03 17:49:55 +0300

    delete home state

--- app/src/main/java/com/example/habbittracker/ui/state/HomeUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.habbittracker.ui.state
-
-import com.example.habbittracker.data.models.Habit
-
-data class HomeUiState(
-    val habits: List<Habit>,
-    val totalXp: Int,
-    val streak: Int
-)
